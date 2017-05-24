77d4844f7b01d1793fa4b314c27998556b17fdc6
Janita <zhuchenjian@huizhi-tech.com>
2017-05-24 09:38:35 +0800

    Initial commit

---
+++ src/main/java/com/janita/Java8CharacterApplication.java
@@ -0,0 +1,12 @@
+package com.janita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8CharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8CharacterApplication.class, args);
+	}
+}
