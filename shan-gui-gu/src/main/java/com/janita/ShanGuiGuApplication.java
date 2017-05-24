1aa39b31a50e324333bb5e49a5ff7d6f095f94c9
janforp <janforp@163.com>
2017-05-24 21:14:21 +0800

    尚硅谷视频教程

---
+++ shan-gui-gu/src/main/java/com/janita/ShanGuiGuApplication.java
@@ -0,0 +1,12 @@
+package com.janita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanGuiGuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShanGuiGuApplication.class, args);
+	}
+}
---
+++ shan-gui-gu/src/test/java/com/janita/ShanGuiGuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.janita;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShanGuiGuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
